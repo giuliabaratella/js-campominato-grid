2aac1028caeeca4addb6dea954e09b7524680816
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-18 15:34:32 +0200

    added reset + console.log (n)

--- js/script.js
+++ js/script.js
@@ -8,23 +8,26 @@ function campoMinato (){
     // console.log (btn, playground);
 
     // preparo la funzione per creare un quadrato 
-    function drawSquare (){
+    function drawSquare(n){
         const square = document.createElement('div');
         square.classList.add ('square');
         // creo l'evento del click sul quadrato
         square.addEventListener('click',function(){
          square.classList.add('active');
+         console.log(n);
         })
         return square;
     }
 
     // creo l'evento del bottone 
     btn.addEventListener('click', function(){
+        // aggiungo il reset 
+        playground.innerHTML = '';
         // definisco quanti quadrati devo creare 
         nSquare = 100;
         //creo un ciclo che per nSquare volte crea i quadrati 
         for (i = 1; i <= nSquare; i++){
-            let square = drawSquare();
+            let square = drawSquare(i);
             playground.append (square);
         }
     }
